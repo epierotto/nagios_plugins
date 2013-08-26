7080042d9ec7bd3f057428acc3f58caa49ccb48f
Enrique Garbi <garbi@nimbuzz.com.ar>
2013-08-26 11:55:02 -0300

    Many bugs and improvements

--- log4j/log4j_parser.pl
+++ log4j/log4j_parser.pl
@@ -1,117 +1,134 @@
-#!/usr/bin/perl -w
+#! /usr/bin/perl -w
 
-use warnings;
-use strict;
-use Getopt::Long qw(:config gnu_getopt);
-use File::Basename;
-use Sys::Hostname;
+use POSIX qw(strftime);
 use Nagios::Plugin;
-my $VERSION="1.2.1";
+
+$VERSION = "2.0.0";
+
+$blurb = "This plugin retrieves data from log lines matching labels and columns in a time frame, also you can set thresholds for each of label.";
+
+$license = "This nagios plugin is free software, and comes with ABSOLUTELY NO WARRANTY. It may be used, redistributed and/or modified under the terms of the GNU General Public Licence (see http://www.fsf.org/licensing/licenses/gpl.txt).";
+
 my $np;
 
-$np = Nagios::Plugin->new(usage => "Usage: %s [--path|-p <path>] [-s|--string <string>] [--column|-C <column>] [-w|--warning <string>] [-c|--critical <string>] [-?|--usage] [-V|--version] [-h|--help] [-v|--verbose] [-t|--timeout=<timeout>]",
-                          version => $VERSION,
-                          blurb => 'This plugin retrieves the values of the given strings from the given log file, also you can check thresholds for each string.http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT',
-                          license => "Brought to you AS IS, WITHOUT WARRANTY, under GPL. (C)",
-                          shortname => "PARSE_LOG",
-                         );
+$np = Nagios::Plugin->new(usage => "Usage: %s [-p|--path <complete path to log file>] [-s|--string <labels to search for>] [-m|--minutes <ammount of minutes before the actual date to set the time frame>] [-C|--column <name of the column to retrieve the data>] [-d|--date-format <date format>] [-n|--column-names <set names for each log column>] [-w|--warning <warning threshold>] [-c|--critical <critical threshold>] [-?|--usage] [-V|--version] [-h|--help] [-t|--timeout=<timeout>]",
+                        version => $VERSION,
+                        blurb   => $blurb,
+                        license => $license,
+                        );
 
 $np->add_arg(spec => 'path|p=s',
-             help => q(Complete path to the log file, indicated in STRING),
+             help => q(Complete path to the log file),
              required => 1,
             );
 
 $np->add_arg(spec => 'string|s=s',
-             help => q(Values or strings to seach for separated with "+", indicated in STRING),
+             help => q(Labels or tags to search in the log file, separated with "+" from each other),
              required => 1,
             );
 
-$np->add_arg(spec => 'column|C=i',
-             help => q(This is the number of column to retrieve the values. Default value is 5),
+$np->add_arg(spec => 'minutes|m=i',
+             help => q(Ammount of minutes before the actual date to set a range of time. Default 1 min),
              required => 0,
-	     default => 5,
+             default => 1,
+            );
+
+$np->add_arg(spec => 'column|C=s',
+             help => q(Name of the column to pick up the value. Default value: Count),
+             required => 0,
+             default => "5",
+            );
+
+$np->add_arg(spec => 'date-format|d=s',
+             help => q(Date format of the log file based on POSIX strftime. Default is "%Y-%m-%d %H:%M:"),
+             required => 0,
+             default => "%Y-%m-%d %H:%M:",
+            );
+
+$np->add_arg(spec => 'column-names|n=s',
+             help => q(You can set names for each log column, separated with "+" from each other. Default value is: 0+1+2+3+4+5),
+             required => 0,
+             default => "0+1+2+3+4+5",
             );
 
 $np->add_arg(spec => 'critical|c=s',
-             help => q(Values for the Critical Thresholds separated with "+". The ammount of given criticals thresholds must match with the ammount of strings to check. http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT),
+             help => q(Values for the Critical Thresholds separated with "+". The ammount of given criticals thresholds must match the ammount of labels to check. http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT),
              required => 0,
             );
 
 $np->add_arg(spec => 'warning|w=s',
-             help => q(Values for the Warning Thresholds separated with "+". The ammount of given warnings thresholds must match with the ammount of strings to check. http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT),
+             help => q(Values for the Warning Thresholds separated with "+". The ammount of given warnings thresholds must match the ammount of labels to check. http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT),
              required => 0,
             );
 
-# Getting the opts
+# Parse and process arguments
 $np->getopts;
-my $ng = $np->opts;
+$ng = $np->opts;
 
 # Manage timeout
 alarm $ng->timeout;
 
-# Verbosity
-my $verbose = $ng->get('verbose');
+# Arguments processing 
+$file = $ng->get('path');
+@LABELS = split(/\+/, $ng->get('string'));
+$minutes_before = $ng->get('minutes');
+$date_format = $ng->get('date-format');
+@column_names = split(/\+/,$ng->get('column-names'));
+$REGEX_columns = join("|", @column_names);
+$REGEX_labels = join("|", @LABELS);
 
-# Options
-my $path = $ng->get('path');
-my @strings = split(/\+/,$ng->get('string'));
-my @warnings;
-my @criticals;
-my $column = $ng->get('column');
-
-# Building the thresholds
-if (defined $ng->get('warning') && $ng->get('critical')) {
-        @warnings = split(/\+/,$ng->get('warning'));
-        @criticals = split(/\+/,$ng->get('critical'));
-eval ($#warnings==$#strings and $#criticals==$#strings) or $np->nagios_exit(CRITICAL,"You should specify the same amount of warnings, criticals and strings.");
+# Building the thresholds if any
+if ($ng->get('warning') or $ng->get('critical')){
+        if ($ng->get('warning')){
+                @warnings = split(/\+/,$ng->get('warning'));
+        }
+        if ($ng->get('critical')){
+                @criticals = split(/\+/,$ng->get('critical'));
+        }
+        eval ($#warnings==$#LABELS and $#criticals==$#LABELS) or $np->nagios_exit( CRITICAL,"You must provide a warning and critical threshold per tag or no thresholds at all, check your thresholds!");
 }
 
-# Taking the date with the right format
-my $date = `date "+%F %H:%M:00" -d "1 min ago"`;
-$date =~s/\n//g;
-# Retrieving the data from the log
-eval (my @lines = `grep -A 200 "$date" $path`) or $np->nagios_exit( CRITICAL, "Unable to get values from $path");
+# Checking performance data columns
+if ($ng->get('column') and $ng->get('column') =~/$REGEX_columns/){
+	$column_selected = $&;
+} else {
+	$np->nagios_exit( CRITICAL,"Chosen column \"".$ng->get('column')."\" not available. Options are: @column_names");
+}
+
+# Date formatting
+$timestamp_before = time - ($minutes_before * 60);
+$before = strftime "$date_format", localtime($timestamp_before);
+$now = strftime "$date_format", localtime;
+
+
+# Reading the file
+open (FILE,$file) or $np->nagios_exit( CRITICAL, "Unable to read $file");
+        while (<FILE>) {
+                next unless /$before\d\d/../$now\d\d/ and /\b$REGEX_labels\b/ and $tag = $&;
+		@COLUMNS{@column_names} = split(/\s+/,$_);
+		$PERFDATA{$tag} = $COLUMNS{$column_selected};
+        }
+close FILE or $np->nagios_exit( CRITICAL, "ERROR while closing $file");
+
 
 # Initial message and status
 my $code = OK;
 my $msg = $np->add_message($code,"All stats retrieved");
 
-# And here we go!
-foreach my $label (@strings){
-	my $value = 0;
-	my $j = 1;
-
-	# Reading the retrieved lines
-	foreach (@lines){
-                my @lineparsed=split(/\s+/,$_);
-                        if (defined ($lineparsed[0]) && $lineparsed[0] eq $label){
-				$value = $lineparsed[$column];
-                                        $np->add_perfdata(
-                                               label => $label,
-                                               value => $value,
-                                        );
-                                        last;
-                        }
-                        else {
-                                if ($j == $#lines){
-					$np->add_perfdata(
-                                               label => $label,
-                                               value => $value,
-                                        );
-                                }
-                        }
-	$j++;
-	}
-
-	# Checking thresholds
-	if (@warnings and @criticals){
-		my $warning = shift @warnings;
-		my $critical = shift @criticals;
-		if (defined $warning and $critical){
-			$code = $np->check_threshold( check => $value, warning => $warning, critical => $critical);
-			$msg = $np->add_message($code,"$label=$value W=$warning C=$critical") if $code != OK;
-		}
-	}
+# Loading retrieved data
+foreach $label (@LABELS){
+        defined $PERFDATA{$label} or $PERFDATA{$label} = 0;
+        $np->add_perfdata(
+                label => $label,
+                value => $PERFDATA{$label},
+                );
+        # Checking thresholds
+        if (@warnings and @criticals){
+                $warning = shift @warnings;
+                $critical = shift @criticals;
+                $code = $np->check_threshold( check => $PERFDATA{$label}, warning => $warning, critical => $critical);
+                $msg = $np->add_message($code,"$label=$PERFDATA{$label} W=$warning C=$critical") if $code != OK;
+                }
 }
 
 # Exit
